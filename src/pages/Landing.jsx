894fbfb64a802150e37d4d2456fa182aa23793bf
dimas <dimmaa9@gmail.com>
2025-01-15 13:16:54 +0200

    (update) build 3

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,11 +5,13 @@ import ManualBlock from "./ManualBlock";
 import FaqBlock from "./FaqBlock";
 import CourseBlock from "./CourseBlock";
 import Footer from "../components/Footer";
+import ExchangeBlock from "./ExchangeBlock";
 
 const Landing = () =>{
     return(
         <div>
             <WelcomeBlock/>
+            <ExchangeBlock/>
             <TradeBlock/>
             <RoadmapBlock currentStage={2}/>
             <ManualBlock/>
